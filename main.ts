4cb7016ec2481a266f8c8714b99cbe9f096d0ae1
VarunTQQber <122548038+VarunTQQber@users.noreply.github.com>
2023-01-13 11:13:30 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Cow)
+    music.playTone(294, music.beat(BeatFraction.Whole))
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.SmallHeart)
+    music.playTone(330, music.beat(BeatFraction.Whole))
 })
 basic.showIcon(IconNames.Pitchfork)
+music.playTone(262, music.beat(BeatFraction.Whole))
